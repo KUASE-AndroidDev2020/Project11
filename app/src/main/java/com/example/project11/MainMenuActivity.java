8da067dbc7d32fbdeb3cfffa5e1211dc0cb2e443
Futo-Fukuyama <65376683+Futo-Fukuyama@users.noreply.github.com>
2020-07-16 15:36:47 +0900

    1, Fixing visual on the emulator 2, Connect activity

--- app/src/main/java/com/example/project11/MainMenuActivity.java
+++ app/src/main/java/com/example/project11/MainMenuActivity.java
@@ -2,7 +2,9 @@ package com.example.project11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void goToSharingDisasterInfoActivity(View view) {
+        Intent toShare = new Intent(this, SharingDisasterInfo.class);
+        startActivity(toShare);
+    }
+
+    public void goToHazardMapActivity(View view) {
+        Intent toHazard = new Intent(this, HazardMap.class);
+        startActivity(toHazard);
+    }
+
+    public void goToCheckListActivity(View view) {
+        Intent toCheckList = new Intent(this, check_list.class);
+        startActivity(toCheckList);
+    }
+
+    public void goToQuizMenuActivity(View view) {
+        Intent toQuizMenu = new Intent(this, QuizMenuActivity.class);
+        startActivity(toQuizMenu);
     }
 }
